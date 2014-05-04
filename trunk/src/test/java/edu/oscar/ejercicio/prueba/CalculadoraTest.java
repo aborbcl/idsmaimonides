f8981d8aae28ad79a4310066294ec47728128919
aborbont@gmail.com <aborbont@gmail.com@d358c7ea-0e6c-20c3-70d9-90402da349ff>
2014-05-04 21:44:42 +0000

    https://maimonides2014ides2.atlassian.net/browse/EAB-1

--- trunk/src/test/java/edu/oscar/ejercicio/prueba/CalculadoraTest.java
+++ trunk/src/test/java/edu/oscar/ejercicio/prueba/CalculadoraTest.java
@@ -6,24 +6,18 @@ import org.junit.Test;
 
 public class CalculadoraTest {
 	
-	
-
 	@Test
 	public void dividirPor0DaNull() {
 		Calculadora calc = new Calculadora();
 		Double x = calc.dividir(1, 0);
 		System.out.println(x);
 		assertNull(x);
-				
 	}
-
 	@Test
 	public void dividirOk() {
 		Calculadora calc = new Calculadora();
 		Double x = calc.dividir(1, 2);
 		System.out.println(x);
 		assertEquals(0.5, x, 0.01);
-				
 	}
-	
 }
