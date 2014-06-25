1b9d66056a3378a9300a547f86f88e42a490ec59
aborbont@gmail.com <aborbont@gmail.com@d358c7ea-0e6c-20c3-70d9-90402da349ff>
2014-06-25 22:42:04 +0000

    revision

--- src/test/java/edu/oscar/ejercicio/prueba/CalculadoraTest.java
+++ src/test/java/edu/oscar/ejercicio/prueba/CalculadoraTest.java
@@ -1,20 +1,46 @@
 package edu.oscar.ejercicio.prueba;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
-public class CalculadoraTest {
+public class CalculadoraTest extends TestCase{
 	
+	public CalculadoraTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+	
+    public void testDividir() throws CalculadoraException{
+        System.out.println("division");
+        double num = 6.0;
+        double denum = 3.0;
+        Calculadora instance = new Calculadora();
+        Double expResult = 2.0;
+        Double result=instance.dividir(num, denum);
+        assertEquals(expResult, result);
+    }
+    
+
 	@Test
-	public void dividirPor0DaNull() {
+	public void dividirPor0DaNull() throws CalculadoraException {
 		Calculadora calc = new Calculadora();
 		Double x = calc.dividir(1, 0);
 		System.out.println(x);
 		assertNull(x);
 	}
 	@Test
-	public void dividirOk() {
+	
+	public void dividirOk() throws CalculadoraException {
 		Calculadora calc = new Calculadora();
 		Double x = calc.dividir(1, 2);
 		System.out.println(x);
